39f5f200305c9d78c288eb2b2a07c2da8da7e713
Sathiyan Sivathas <sathiyan.sivathas@metaswitch.com>
2018-01-16 12:47:54 +0000

    Add Mock S4 API

---
+++ src/ut/mock_s4.h
@@ -0,0 +1,40 @@
+/**
+ * @file mock_s4.h
+ *
+ * Copyright (C) Metaswitch Networks 2018
+ * If license terms are provided to you in a COPYING file in the root directory
+ * of the source code repository by which you are accessing this code, then
+ * the license outlined in that COPYING file applies to your use.
+ * Otherwise no rights are granted except for those provided to you by
+ * Metaswitch Networks in a separate written agreement.
+ */
+
+#ifndef MOCK_S4_H__
+#define MOCK_S4_H__
+
+#include "gmock/gmock.h"
+#include "s4.h"
+
+
+class MockS4 : public S4
+{
+  MockS4();
+  virtual ~MockS4();
+
+  MOCK_METHOD3(handle_get, HTTPCode(std::string aor_id,
+                                    AoR** aor,
+                                    SAS::TrailId trail));
+
+  MOCK_METHOD2(handle_delete, HTTPCode(std::string aor_id,
+                                       SAS::TrailId trail));
+
+  MOCK_METHOD3(handle_put, HTTPCode(std::string aor_id,
+                                    AoR* aor,
+                                    SAS::TrailId id));
+
+  MOCK_METHOD3(handle_patch, HTTPCode(std::string aor_id,
+                                      PatchObject* patch_object,
+                                      SAS::TrailId trail));
+};
+
+#endif
