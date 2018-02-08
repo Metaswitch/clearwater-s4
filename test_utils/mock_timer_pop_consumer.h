652bb089b5e065ea0bee665e4e7285b9fe3bbbad
Alex Hockey <alex.hockey@metaswitch.com>
2018-02-08 16:30:55 +0000

    Make timer pop consumer into it's own interface

---
+++ test_utils/mock_timer_pop_consumer.h
@@ -0,0 +1,22 @@
+/**
+ * @file mock_timer_pop_consumer.h
+ *
+ * Copyright (C) Metaswitch Networks 2018
+ * If license terms are provided to you in a COPYING file in the root directory
+ * of the source code repository by which you are accessing this code, then
+ * the license outlined in that COPYING file applies to your use.
+ * Otherwise no rights are granted except for those provided to you by
+ * Metaswitch Networks in a separate written agreement.
+ */
+
+#ifndef MOCK_TIMER_POP_CONSUMER_H__
+#define MOCK_TIMER_POP_CONSUMER_H__
+
+class MockTimerPopConsumer : public S4::TimerPopConsumer
+{
+public:
+  MOCK_METHOD2(handle_timer_pop, void(const std::string&, SAS::TrailId));
+}
+
+#endif
+
